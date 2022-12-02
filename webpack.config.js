36082c9e123a14dac85f9b90acb49c17051f2e9f
lllAR10 <lllariogonzalez@gmail.com>
2022-12-02 15:06:01 -0300

    portfolio lllariogonzalez

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    entry: './public/script.js',
-    output: {
-        path: __dirname + '/public/dist',
-        filename: 'bundle.js',
-    },
-    devtool: 'eval-source-map',
+  entry: './public/script.js',
+  output: {
+    // eslint-disable-next-line n/no-path-concat
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map'
 }
