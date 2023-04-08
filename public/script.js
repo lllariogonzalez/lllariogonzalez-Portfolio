820135112fb152039e4eafafefa3b0ecded443d7
lllariogonzalez <lllariogonzalez@gmail.com>
2023-04-08 11:23:04 -0300

    web under construction

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const SQUARES = 3000
 
-for(let i = 0; i < SQUARES; i++) {
+for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
@@ -14,14 +14,14 @@ for(let i = 0; i < SQUARES; i++) {
 }
 
 function setColor(element) {
-   const color = getRandomColor()
-   element.style.background = color
-   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
-   element.style.background = '#1d1d1d'
-   element.style.boxShadow = '0 0 2px #000'
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
 }
 
 function getRandomColor() {
