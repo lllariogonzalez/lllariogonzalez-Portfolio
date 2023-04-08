2574dd56c4a36b37a78e472a3cf8bae01c839bfd
Mario Gonzalez <lllariogonzalez@gmail.com>
2023-04-08 11:34:02 -0300

    add event preventDefault

--- public/script.js
+++ public/script.js
@@ -18,12 +18,14 @@ for (let i = 0; i < SQUARES; i++) {
 }
 
 function setColor(element) {
+    event.preventDefault ()
     const color = getRandomColor()
     element.style.background = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
+    event.preventDefault ()
     element.style.background = '#1d1d1d'
     element.style.boxShadow = '0 0 2px #000'
 }
